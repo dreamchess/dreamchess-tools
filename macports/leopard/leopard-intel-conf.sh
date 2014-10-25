a09d3f62b97295e752097daa6c8b1c326979c5eb
Walter van Niftrik <walter@vanniftrik-it.nl>
2014-10-25 12:53:10 +0200

    Remove old macports tree

--- macports/leopard/leopard-intel-conf.sh
+++
@@ -1,3 +0,0 @@
-CFLAGS="-I/opt/local/include -L/opt/local/lib -mmacosx-version-min=10.4 \
--isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -force_cpusubtype_ALL" \
-./configure --disable-dependency-tracking
